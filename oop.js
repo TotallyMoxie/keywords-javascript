58ec01c30ae2f33bd711965a5da7d0f9018d5d47
TotallyMoxie <134233554+TotallyMoxie@users.noreply.github.com>
2023-09-10 15:15:55 -0500

    added darth vader saying and class property

--- oop.js
+++ oop.js
@@ -1,9 +1,11 @@
 class Player { // class is a blueprint for creating objects, this is a class constructor
+  age = 20; //this is a class property
   constructor(name, hp, mp, items) {
     this.name = name;
     this.hp = hp;
     this.mp = mp;
     this.items = items;
+    //this.age = 30; //this is a class property, and same as line 2
   }
 
   speak (phrase) {  //this is a class method
@@ -30,8 +32,13 @@ function AnotherPlayer(name, hp, mp, items) { //this is also a class constructor
   this.items = items;
 }
 
+AnotherPlayer.prototype.speak = function(phrase) { //this is a class method
+  console.log(`${this.name} says "${phrase}"`);
+};
+
 const darthVader = new AnotherPlayer("Darth Vader", 200, 50, ["lightsaber"]);
 console.log(darthVader);
+darthVader.speak("Luke, I am your father!"); //Darth Vader says "Luke, I am your father!"
 console.log(Object.getOwnPropertyNames(darthVader));
 console.log(Object.getOwnPropertyNames(darthVader.__proto__));
 
